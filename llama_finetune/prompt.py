ef69e27a88d2d82b58af825c76d48d8b0577394b
Naomi Leow <naomileowwx@gmail.com>
2023-10-31 23:13:34 +0800

    Fix prompt for training

--- llama_finetune/prompt.py
+++ llama_finetune/prompt.py
@@ -1,42 +1,55 @@
 def build_table_row(row):
     return f"||{'|'.join(row)}||"
 
-def get_caption(data):
+
+def get_field(data, field, index=None):
+    res = data[field]
+    if index is not None:
+        res = res[index]
+    return res
+
+def get_caption(data, index=None):
     if 'table_caption' not in data:
         return []
-    if data['table_caption'] is None:
+    caption = get_field(data, 'table_caption', index)
+    if caption is None:
         return []
     return [
-        f"Caption: {data['table_caption']}"
+        f"Caption: {caption}"
     ]
 
-def format_prompt(data):
-    table_header = build_table_row(data['table_column_names'])
-    table_body = [build_table_row(r) for r in data['table_content_values']]
+def format_prompt(data, index=None):
+    table_header = build_table_row(get_field(data, 'table_column_names', index))
+    table_body = [build_table_row(r) for r in get_field(data, 'table_content_values', index)]
     lines = [
         'Read the Instruction below and provide an answer.',
         '### INSTRUCTION:\nRead the following table and answer a question\n',
-        *get_caption(data),
+        *get_caption(data, index),
         'Table:',
         table_header,
         *table_body,
-        *format_context(data),
+        *format_context(data, index),
         '\nQuestion:',
-        data['question'],
+        get_field(data, 'question', index),
         '\n### Response:'
     ]
 
     return '\n'.join(lines)
 
-def format_answer(data):
-    f"The answer is {data['answer']}"
+def format_prompt_with_answer(data, index=None):
+    prompt = format_prompt(data, index)
+    answer = format_answer(data, index)
+    return f"{prompt}\n{answer}"
+
+def format_answer(data, index=None):
+    f"The answer is {get_field(data, 'answer', index)}"
 
 def filter_empty_sentences(rows):
     return [r for r in rows if r != '.']
 
-def format_context(data):
-    pre = filter_empty_sentences(data['pre_text']) if 'pre_text' in data else []
-    post = filter_empty_sentences(data['post_text']) if 'post_text' in data else []
+def format_context(data, index=None):
+    pre = filter_empty_sentences(get_field(data, 'pre_text', index)) if 'pre_text' in data else []
+    post = filter_empty_sentences(get_field(data, 'post_text', index)) if 'post_text' in data else []
     total_len = len(pre)+len(post)
     if total_len == 0:
         return []
@@ -48,8 +61,11 @@ def format_context(data):
     
     return lines
 
-def formatting_prompts_func(data):    
-    return [format_prompt(d) for d in data]
+def formatting_prompts_func(data):
+    out = []
+    for i in range(len(data['question'])):
+        out.append(format_prompt_with_answer(data, i))
+    return out
 
 # Usage with trainer
 # trainer = SFTTrainer(
